bfa73084dd8f07eb813f56fe90203e6d6a09cf05
jens1o <jenshausdorf@gmail.com>
2018-06-16 17:27:57 +0200

    fixed sql syntax lol

--- src/routes/trashcan.js
+++ src/routes/trashcan.js
@@ -15,15 +15,18 @@ router.get('', (request, response) => {
              '* COS(RADIANS(? - trash_bins.longitude)) ' +
              '+ SIN(RADIANS(?)) ' +
              '* SIN(RADIANS(trash_bins.latitude)))) AS distance_in_km ' +
-             'FROM trash_bins ';
+             'FROM trash_bins ' +
              'HAVING distance_in_km <= ? ' +
-             'ORDER BY distance_in_km DESC ' +
-             'LIMIT 10';
+             'ORDER BY distance_in_km ASC ' +
+             'LIMIT 10;';
         let parameters = [latitude, longitude, latitude, Number.parseFloat(process.env.DISTANCE_THRESHOLD)];
-
-        DatabaseHandler.getInstance().getDatabase().query(sql, parameters, ());
+        DatabaseHandler.getInstance().getDatabase().query(sql, parameters, (error, results) => {
+            if (error) throw error;
+            response.json(results);
+        });
+    } else {
+        response.status(401).json('You need to provide your current position.');
     }
-    response.end('tbd');
 });
 
 module.exports = router;
