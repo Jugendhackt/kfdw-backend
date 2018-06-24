94f589cdc43aad7cf4abd1103b190dd98d3dfeb7
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2018-06-24 23:24:51 +0200

    Add some linting

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 
 const DatabaseManager = require('./database').getInstance();
 const logger = require('./logger');
+
 const MainLogger = logger.getInstance('Main');
 const ServerLogger = logger.getInstance('Server');
 
@@ -14,7 +15,7 @@ const TrashcanRouteHandler = require('./routes/trashcan');
 const ScoreRouteHandler = require('./routes/score');
 
 // Use environment port specification if set; if not, use port 8080 (makes app compatible with heroku)
-const PORT = Number.parseInt(process.env.PORT) || 8080
+const PORT = Number.parseInt(process.env.PORT) || 8080;
 
 MainLogger.log(`${require('../package').name} started at ${new Date().toLocaleTimeString()}`);
 
@@ -28,27 +29,27 @@ app.disable('x-powered-by');
 /**
  * @type Map<string,int>
  */
-const ipAddressesToCount = new Map;
+const ipAddressesToCount = new Map();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: true
+  extended: true,
 }));
 
 app.use((request, response, next) => {
-    // allow CORS
-    response
-        .header('Access-Control-Allow-Origin', '*')
-        .header('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE');
+  // allow CORS
+  response
+    .header('Access-Control-Allow-Origin', '*')
+    .header('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE');
 
-    let counter = (ipAddressesToCount.get(request.ip) || 0);
-    ipAddressesToCount.set(request.ip, ++counter);
+  let counter = (ipAddressesToCount.get(request.ip) || 0);
+  ipAddressesToCount.set(request.ip, ++counter);
 
-    // log the request
-    MainLogger.log(
-        `New ${request.method} request on ${request.originalUrl} from ip ${request.ip} at ${new Date().toLocaleTimeString()} (${ipAddressesToCount.get(request.ip)} time(s))`
-    );
-    next();
+  // log the request
+  MainLogger.log(
+    `New ${request.method} request on ${request.originalUrl} from ip ${request.ip} at ${new Date().toLocaleTimeString()} (${ipAddressesToCount.get(request.ip)} time(s))`,
+  );
+  next();
 });
 
 app.use('/trashcans', TrashcanRouteHandler);
@@ -56,16 +57,16 @@ app.use('/trash', TrashRouteHandler);
 app.use('/score', ScoreRouteHandler);
 
 app.use('/', (request, response) => {
-    response.json({
-        message: 'It workz :P'
-    });
+  response.json({
+    message: 'It workz :P',
+  });
 });
 
 const runningInstance = app.listen(PORT, () => {
-    ServerLogger.log(`Server is listening on port ${runningInstance.address().port}`);
+  ServerLogger.log(`Server is listening on port ${runningInstance.address().port}`);
 });
 
 process.on('unhandledRejection', (reason, p) => {
-    console.log('Unhandled Rejection at:', p, 'reason:', reason);
-    process.exit(1);
+  console.log('Unhandled Rejection at:', p, 'reason:', reason);
+  process.exit(1);
 });
--- src/routes/score.js
+++ src/routes/score.js
@@ -2,19 +2,18 @@ const router = require('express').Router();
 const DatabaseManager = require('../database').getInstance();
 
 router.get('', (request, response) => {
-    console.log(request.params);
-    if (!request.query.username) {
-        response.status(400).json('You need to provide your username.');
-        return;
-    }
+  console.log(request.params);
+  if (!request.query.username) {
+    response.status(400).json('You need to provide your username.');
+    return;
+  }
 
-    let sql = 'SELECT COUNT(*) FROM trash ' +
-            'WHERE username = ?;'
+  const sql = 'SELECT COUNT(*) FROM trash '
+            + 'WHERE username = ?;';
 
-    DatabaseManager.getDatabase().query(sql, request.query.username, (results) => {
-        response.json({score: results})
-    });
-
+  DatabaseManager.getDatabase().query(sql, request.query.username, (results) => {
+    response.json({ score: results });
+  });
 });
 
 module.exports = router;
