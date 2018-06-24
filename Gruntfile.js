fa6ab15c1925f7b13e6dd2805eb289dcc0a10c11
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2018-06-24 23:49:13 +0200

    Gruntify

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,12 @@
 module.exports = function(grunt) {
+    require('load-grunt-tasks')(grunt);
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-nsp');
     grunt.initConfig({
+	eslint: {
+	    target: ['src/**', 'data/**/*.js'],
+	    failOnError: false
+	},
         jshint: {
             files: ['Gruntfile.js', 'src/**/*.js', 'data/**/*.js'],
             options: {
--- script/citest.sh
+++ script/citest.sh
@@ -3,4 +3,5 @@ grunt jshint
 #eslint src/** ; exit 0
 #eslint data/** ; exit 0
 grunt nsp
+grunt eslint
 yarn run import area=Hessen
